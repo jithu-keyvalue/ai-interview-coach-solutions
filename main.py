1e5a4f0bb0627096353615ca76553f0eedd4bfee
jithu-keyvalue <jithu@keyvalue.systems>
2025-03-23 17:07:14 +0530

    update

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from datetime import datetime
 
 app = FastAPI()
 
-@app.get("/api/time")
-def get_time(name: str = ''):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return { "message": f"Hi {name}! It's {current_time}" }
+users = {}
+
+@app.post("/api/users")
+def create_user(data: dict):
+    user_id = len(users) + 1
+    users[user_id] = data
+    return {"id": user_id, "message": "User profile saved!"}
+
+@app.get("/api/users/{id}")
+def get_user(id: int):
+    return users.get(id, {"error": "User not found"})
