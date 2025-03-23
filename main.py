450272b1ef2c9261e383ea6826098663dfb4be23
jithu-keyvalue <jithu@keyvalue.systems>
2025-03-23 12:00:16 +0530

    update

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from datetime import datetime
+import datetime
 
 app = FastAPI()
 
+@app.get("/")
+def hello_world():
+    return "Hello world!"
+
+
 @app.get("/api/time")
 def get_time():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.datetime.now()
     return { "time": current_time }
